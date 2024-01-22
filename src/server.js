26122fe4cc1f50a9e0fcfa6ae7ff7f53810b8a23
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-01-22 23:48:43 +0100

    end points created on server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,38 @@
-const express = require('express')
+const express = require("express");
+let { cartItems } = require("./temp-data");
+let { products } = require("./temp-data");
 
 const app = express();
+app.use(express.json()); 
 
-app.get('/hello', (req, res) => {
-    res.send("hello ")
-})
+app.get("/hello", (req, res) => {
+  res.send("hello bitch");
+});
+app.get("/products", (req, res) => {
+  res.json(products);
+});
+app.get("/cart", (req, res) => {
+  res.json(cartItems);
+});
+app.get("/products/:productId", (req, res) => {
+  const productId = req.params.productId;
+  const product = products.find((product) => product.id === productId);
+  res.json(product);
+});
+
+app.post("/cart", (req, res) => {
+  const productId = req.body.id;
+  const product = products.find((product) => product.id === productId);
+  cartItems.push(product);
+  res.json(cartItems);
+});
+
+app.delete("/cart/:productId", (req, res) => {
+  const productId = req.params.productId;
+  cartItems = cartItems.filter((product) => product.id !== productId);
+  res.json(cartItems);
+});
 
 app.listen(8000, () => {
-    console.log("Server is listening on port 8000");
-})
+  console.log("Server is listening on port 8000");
+});
