23afdd709b618e702927a4ac4a5954c7295db070
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2024-01-23 03:53:36 +0100

    end points refactored on server.js

--- src/server.js
+++ src/server.js
@@ -3,17 +3,21 @@ let { cartItems } = require("./temp-data");
 let { products } = require("./temp-data");
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 
-app.get("/hello", (req, res) => {
-  res.send("hello bitch");
-});
 app.get("/products", (req, res) => {
   res.json(products);
 });
+
+function populatedCartIds(ids) {
+  return ids.map((id) => products.find((product) => product.id === id));
+}
+
 app.get("/cart", (req, res) => {
-  res.json(cartItems);
+  const populatedCart = populatedCartIds(cartItems)
+  res.json(populatedCart);
 });
+
 app.get("/products/:productId", (req, res) => {
   const productId = req.params.productId;
   const product = products.find((product) => product.id === productId);
@@ -22,15 +26,16 @@ app.get("/products/:productId", (req, res) => {
 
 app.post("/cart", (req, res) => {
   const productId = req.body.id;
-  const product = products.find((product) => product.id === productId);
-  cartItems.push(product);
-  res.json(cartItems);
+  cartItems.push(productId);
+  const populatedCart = populatedCartIds(cartItems)
+  res.json(populatedCart);
 });
 
 app.delete("/cart/:productId", (req, res) => {
   const productId = req.params.productId;
-  cartItems = cartItems.filter((product) => product.id !== productId);
-  res.json(cartItems);
+  cartItems = cartItems.filter((id) => id !== productId);
+  const populatedCart = populatedCartIds(cartItems);
+  res.json(populatedCart);
 });
 
 app.listen(8000, () => {
